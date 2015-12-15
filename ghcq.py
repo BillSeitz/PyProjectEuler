938e98d213d155180f2df887222313e6a87bc176
Bill Seitz <bill@fluxent.com>
2015-12-14 22:10:54 -0600

    Successfully printing out rows, and generating left-most solution for a row.

--- ghcq.py
+++ ghcq.py
@@ -11,8 +11,12 @@ Remember to always use 0 as first index!
 grid_size = 25 # 0...24
 grid_max = grid_size - 1
 sizes = {}
-sizes[1] = (7,3,1,1,7)
+sizes[0] = (7,3,1,1,7)
+sizes[1] = (1,1,2,2,1,1)
 
+def show_header(): #bah just fixed for 25 right now
+	print '0123456789012345678901234'
+	
 def show_row(size_list, starts_list):
 	"""
 	output row as series of characters
@@ -23,11 +27,11 @@ def show_row(size_list, starts_list):
 	for i in range(0, len(size_list)):
 		if starts_list[i] > c: # next start is after current row
 			out = out + ('0' * (starts_list[i] - c))
-			out = out + ('1' * size_list[i])
-			c = starts_list[i] + size_list[i]
-		if c < 25:
-			out = out + ('0' * (25 - c))
-	print out		
+		out = out + ('1' * size_list[i])
+		c = starts_list[i] + size_list[i]
+	if c < 25:
+		out = out + ('0' * (25 - c))
+	return out		
 
 def leftest(size_list):
 	"""
@@ -38,7 +42,7 @@ def leftest(size_list):
 	for run in size_list:
 		starts_list.append(c)
 		c = c + run + 1
-	print starts_list
+	#print starts_list
 	return starts_list
 
 def next(starts_list):
@@ -54,6 +58,8 @@ def next(starts_list):
 	
 	
 if __name__ == '__main__':
+	show_header()
+	print show_row(sizes[0], leftest(sizes[0]))
 	print show_row(sizes[1], leftest(sizes[1]))
 
 
