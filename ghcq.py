9c8d2eda105ab9c44c8cc4609fdc25f96229f827
Bill Seitz <bill@fluxent.com>
2015-12-16 11:11:43 -0600

    Probably about to do some refactoring...

--- ghcq.py
+++ ghcq.py
@@ -6,6 +6,14 @@ sizes = {row_num:(len1, len2...)}
 starts = {row_num: (start1, start2...)}
 
 Remember to always use 0 as first index!
+
+Basic process:
+* start with every row having its runs pushed to the far left
+* treat top row like ones-digit, 2nd row like tens-digit, etc.
+* so start by testing all the row_next variations of the first row, then
+ * bump the 2nd row row_next once and push the 1st row back to leftest, testing all the first-row variations, then
+ * rest of 2nd/1st row variations combos, then
+ * bump the 3rd row row_next once, etc.
 """
 
 grid_size = 25 # 0...24
@@ -68,12 +76,14 @@ def show_header(): #bah just fixed for 25 right now
     
 def row(size_list, starts_list):
     """
-    output row as series of characters
+    return row as series of characters
     """
     #out = '0' * grid_size
     out = ''
     c = 0
+    #print 'sizes, starts:', size_list, starts_list
     for i in range(0, len(size_list)):
+    	#print 'c, i, starts[i]:', c, i, starts_list
         if starts_list[i] > c: # next start is after current row
             out = out + ('0' * (starts_list[i] - c))
         out = out + ('1' * size_list[i])
@@ -84,7 +94,7 @@ def row(size_list, starts_list):
 
 def leftest(size_list):
     """
-    Return "solution" (list of starts) for a row that puts each run as far left as possible
+    Return "row-solution" (list of starts) for a row that puts each run as far left as possible
     """
     starts_list = []
     c = 0
@@ -93,22 +103,43 @@ def leftest(size_list):
         c = c + run + 1
     #print starts_list
     return starts_list
+    
+def is_rightest(size_list, starts_list):
+	"""
+	Returns True if all the non-rightest runs are jammed as far right against the rightest one as possible.
+	"""
+	l = len(size_list)
+	rightest_start = starts_list[-1]
+	leftest_start = starts_list[0]
+	sum_left_lengths = sum(size_list[0:-1]) + (l-1) # sum of lengths (excl rightest) plus spaces
+	if rightest_start - leftest_start > sum_left_lengths:
+		return False
+	else:
+		return True
 
-def next(size_list, starts_list):
+def next_row(size_list, starts_list):
     """
-    Return "next solution" list of starts after current solution
+    Return "next row-solution" list of starts after current solution
     Do this recursively:
     * start at right-most run
     * if rest of runs are jammed as far right as possible against it..
-     * then bump them back to far-left and move right-most 1 more to the right
+     * then bump them back to far-left and move right-most 1 more to the right (unless it's already as far right as possible)
     * else:
-     * do next(all but rightmost)
+     * do next_row(all but rightmost) (ooh recursive!)
     """
-    return False
+    if is_rightest(size_list, starts_list):
+    	if start_list[-1] + size_list[-1] == grid_size:
+    		return False
+    	new_starts_list = leftest(size_list[0:-1])
+    	new_starts_list.append(starts_list[-1] + 1)
+    else:
+    	new_starts_list = next_row(size_list[0:-1], starts_list[0:-1])
+    	new_starts_list.append(starts_list[-1])
+    return new_starts_list
     
 def first_solution():
     """
-    Make the first global solution of using leftest() for every row
+    Make the first grid-solution of using leftest() for every row
     """
     rows = {}
     for i in range(0, grid_max):
@@ -171,15 +202,31 @@ def test_solution(solution):
 			return False
 	return True
     
+def run_solutions():
+	i = 0
+	row_to_shift = 0
+	solution = first_solution()
+	while not test_solution(solution):
+		print i, 'fail'
+		i = i+1
+		shifted_row = next_row(sizes[row_to_shift], solution[row_to_shift])
+		if not shifted_row: # meaning returned False, so all-right, so on to next row
+			rows[row_to_shift] = row(sizes[row_to_shift], leftest(sizes[row_to_shift]))
+			row_to_shift = row_to_shift + 1
+			shifted_row = next_row(sizes[row_to_shift], solution[row_to_shift])
+	print i, '------------- Success ------------'
+	display(solution)
+		
+
 if __name__ == '__main__':
     show_header()
-    #print row(sizes[0], leftest(sizes[0]))
+    print row(sizes[0], leftest(sizes[0]))
     #print row(sizes[1], leftest(sizes[1]))
     #display(first_solution())
     #print col(1, first_solution())
     #print test_col(1, col(1, first_solution()))
-    print test_solution(first_solution())
-    #print row(sizes[0], next(leftest(sizes[0])))
+    #print test_solution(first_solution())
+    print row(sizes[0], next_row(sizes[0], leftest(sizes[0])))
 
 
     
